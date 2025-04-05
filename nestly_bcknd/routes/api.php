a2891da106e88b4c3aa292400c94a67a6e354586
Michael <l.goam040930@itses.edu.mx>
2025-04-05 17:20:54 -0600

    api2

--- nestly_bcknd/routes/api.php
+++ nestly_bcknd/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PropiedadController;  
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
@@ -17,4 +18,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('users/{id}', [UserController::class, 'update']);
     Route::delete('users/{id}', [UserController::class, 'destroy']);
     
+    // Rutas de propiedades
+    Route::post('propiedades', [PropiedadController::class, 'store']);  
+    Route::get('propiedades', [PropiedadController::class, 'index']);  
+    Route::get('propiedades/{id}', [PropiedadController::class, 'show']);  
+    Route::put('propiedades/{id}', [PropiedadController::class, 'update']);  
+    Route::delete('propiedades/{id}', [PropiedadController::class, 'destroy']);  
 });
